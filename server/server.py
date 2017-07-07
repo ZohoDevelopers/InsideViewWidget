e2d2e0b11c7266dce85e11823d6fd251847144e4
udhayakumar-uk <uksr94@gmail.com>
2017-07-07 14:45:20 +0530

    new files added

--- server/server.py
+++
@@ -1,6 +0,0 @@
-import BaseHTTPServer, SimpleHTTPServer
-import ssl
-
-httpd = BaseHTTPServer.HTTPServer(('localhost', 8443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='server/server.pem', server_side=True)
-httpd.serve_forever()
